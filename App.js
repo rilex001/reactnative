b8246c2bd3200c320b73457251b315607b367479
rilex001 <ristic1900@gmail.com>
2020-07-12 22:36:10 +0200

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState(initialState)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello, World</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.boldText}>Lorem ipsum dolar si amet. </Text>
+        <Text>Lorem ipsum dolar si amet. </Text>
+        <Text>Lorem ipsum dolar si amet. </Text>
+        <Text>Lorem ipsum dolar si amet. </Text>
+      </View>
     </View>
   );
 }
@@ -18,4 +26,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20
+  },
+  boldText: {
+    fontWeight: 'bold'
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20
+  }
 });
